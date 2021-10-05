68f63deb9ab3c59800b91b02c583415f8dc27fa3
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-06 00:48:50 +0300

    Add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render() {
+        return (
+            <button onClick={this.props.action}>{this.props.title}</button>
+        );
+    }
+}
+export default Button;
